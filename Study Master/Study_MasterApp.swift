4b7e8e10aac57cbc5eadc5a208f67b773a886444
RoomDoom01 <finleywroom@gmail.com>
2025-07-08 20:05:13 -0700

    initial-UI-commit+Github-reconfig

---
+++ Study Master/Study_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_MasterApp.swift
+//  Study Master
+//
+//  Created by Finley Room on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Study_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Study MasterTests/Study_MasterTests.swift
@@ -0,0 +1,16 @@
+//
+//  Study_MasterTests.swift
+//  Study MasterTests
+//
+//  Created by Finley Room on 7/2/25.
+//
+
+import Testing
+
+struct Study_MasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
